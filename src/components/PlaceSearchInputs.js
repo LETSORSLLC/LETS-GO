85a92dcf80cab8abf3bb7eb6343861d6ed8a08bd
DON BADSHA <DON@letsgors.com>
2024-12-11 21:31:20 -0500

    Update PlaceSearchInputs.js
    Signed-off-by: DON BADSHA <DON@letsgors.com>

--- src/components/PlaceSearchInputs.js
+++ src/components/PlaceSearchInputs.js
@@ -1,4 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import PlaceSearchInputs from './PlaceSearchInputs';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+const store = mockStore({
+  userLocation: { fullAddress: '123 Test St' },
+  nav: { origin: null, destination: null }
+});
+
+describe('PlaceSearchInputs', () => {
+  it('renders correctly', () => {
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <PlaceSearchInputs />
+      </Provider>
+    );
+    expect(getByPlaceholderText('Enter pickup location')).toBeTruthy();
+    expect(getByPlaceholderText('Where To?')).toBeTruthy();
+  });
+
+  it('sets origin and destination', () => {
+    const { getByPlaceholderText } = render(
+      <Provider store={store}>
+        <PlaceSearchInputs />
+      </Provider>
+    );
+    fireEvent.changeText(getByPlaceholderText('Enter pickup location'), 'Test Origin');
+    fireEvent.changeText(getByPlaceholderText('Where To?'), 'Test Destination');
+  });
+});import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { FAB, LinearProgress } from "react-native-elements";
